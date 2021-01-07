73c71d17a424dab6a4020a9ce45de0e17b04f58e
songhee <thdgml5537@gmail.com>
2021-01-06 23:21:43 -0500

    create view routes and connect

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+app.use(require("./routes/view.js"));
 
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
       useNewUrlParser: true,
@@ -16,6 +17,14 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout", {
       useFindAndModify: false,
       useCreateIndex: true
 });
+const connection = mongoose.connection;
+
+connection.on("connected", () =>{
+    console.log("Mongoose connected successfully")
+})
+connection.on("error", (err)=>{
+    console.log("Mongoose connection error: " + err);
+})
 
 app.listen(PORT, ()=>{
     console.log(`app running on port ${PORT}`);
